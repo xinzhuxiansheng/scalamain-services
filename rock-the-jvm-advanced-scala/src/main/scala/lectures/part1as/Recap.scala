9cac6b698d7f60d9fe5e67a4506c42b122b48343
阿洋 <xinzhuxiansheng@126.com>
2022-10-02 16:36:29 +0800

    add

--- rock-the-jvm-advanced-scala/src/main/scala/lectures/part1as/Recap.scala
+++ rock-the-jvm-advanced-scala/src/main/scala/lectures/part1as/Recap.scala
@@ -77,10 +77,37 @@ object Recap extends App {
   }
 
   incrementer(1)
-  val anonymousIncrementer = (x:Int) => x+1
-  List(1,2,3).map(anonymousIncrementer) // HOF
+
+  val anonymousIncrementer = (x: Int) => x + 1
+  List(1, 2, 3).map(anonymousIncrementer) // HOF
   // map,flatMap,filter
 
   // for-comprehension
-  val
+  val pairs = for {
+    num <- List(1, 2, 3) //if condition
+    char <- List('a', 'b', 'c')
+  } yield num + "-" + char
+
+  // scala collections: Seqs, Arrays, Lists, Vectors, Maps, Tuples
+  val aMap = Map(
+    "Daniel" -> 789,
+    "Jess" -> 555
+  )
+
+  // collections: Options, Try
+  val anOption = Some(2)
+
+  // pattern matching
+  val x = 2
+  val order = x match {
+    case 1 => "first"
+    case 2 => "second"
+    case 3 => "third"
+    case _ => x + "th"
+  }
+
+  val bob = Person("Bob", 22)
+  val greeting = bob match {
+    case Person(n, _) => s"Hi,my name is $n"
+  }
 }
