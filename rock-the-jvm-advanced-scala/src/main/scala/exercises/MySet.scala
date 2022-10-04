e502ddef782272372cd9ed1fbac1095ca6530ee9
阿洋 <xinzhuxiansheng@126.com>
2022-10-04 17:19:55 +0800

    add

--- rock-the-jvm-advanced-scala/src/main/scala/exercises/MySet.scala
+++ rock-the-jvm-advanced-scala/src/main/scala/exercises/MySet.scala
@@ -22,9 +22,25 @@ trait MySet[A] extends (A => Boolean) {
   def filter(predicate: A => Boolean): MySet[A]
 
   def foreach(f: A => Unit): Unit
+
+  /*
+    EXERCISE
+    - removing an element
+    - intersection with another set
+    - difference with another set
+   */
+  def -(elem: A): MySet[A]
+
+  def --(anotherSet: MySet[A]): MySet[A] // difference
+
+  def &(anotherSet: MySet[A]): MySet[A] // intersection
+
+  // EXERCISE #3 - implement a unary_! = NEGATION of a set
+  // set[1,2,3] =>
+  def unary_! : MySet[A]
 }
 
-case class EmptySet[A] extends MySet[A] {
+class EmptySet[A] extends MySet[A] {
   override def contains(elem: A): Boolean = false
 
   override def +(elem: A): MySet[A] = new NonEmptySet[A](elem, this)
@@ -38,6 +54,47 @@ case class EmptySet[A] extends MySet[A] {
   override def filter(predicate: A => Boolean): MySet[A] = this
 
   override def foreach(f: A => Unit): Unit = ()
+
+  override def -(elem: A): MySet[A] = this
+
+  override def --(anotherSet: MySet[A]): MySet[A] = this
+
+  override def &(anotherSet: MySet[A]): MySet[A] = this
+
+  def unary_! : MySet[A] = new PropertyBasedSet[A](_ => true)
+}
+
+// all elements of type A which satisfy a property
+// { x in A | property(x) }
+class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
+  def contains(elem: A): Boolean = property(elem)
+
+  // { x in A | property(x) } + element = { x in A | property(x) || x == element }
+  def +(elem: A): MySet[A] =
+    new PropertyBasedSet[A](x => property(x) || x == elem)
+
+  // { x in A | property(x) } ++ set = { x in A | property(x) || set contains x }
+  def ++(anotherSet: MySet[A]): MySet[A] =
+    new PropertyBasedSet[A](x => property(x) || anotherSet(x))
+
+  // all integers => (_%3) => [0 1 2]
+  def map[B](f: A => B): MySet[B] = politelyFail
+
+  def flatMap[B](f: A => MySet[B]): MySet[B] = politelyFail
+
+  def filter(predicate: A => Boolean): MySet[A] = new PropertyBasedSet[A](x => property(x) && predicate(x))
+
+  def foreach(f: A => Unit): Unit = politelyFail
+
+  def -(elem: A): MySet[A] = filter(x => x != elem)
+
+  def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+
+  def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+  def unary_! : MySet[A] = new PropertyBasedSet[A](x => !property(x))
+
+  def politelyFail = throw new IllegalArgumentException("Really deep rabbit hole!")
 }
 
 class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
@@ -72,12 +129,29 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
     f(head)
     tail foreach f
   }
+
+  override def -(elem: A): MySet[A] = {
+    if (head == elem) tail
+    else tail - elem + head
+  }
+
+  override def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+
+  // override def &(anotherSet: MySet[A]): MySet[A] = filter(x=>anotherSet.contains(x))
+  // filter(anotherSet) = filter(x=>anotherSet.contains(x))
+  override def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet) // intersection = filtering!
+
+  // new operator
+  def unary_! : MySet[A] = new PropertyBasedSet[A](x => !this.contains(x))
 }
 
 object MySet {
   /*
     val s = MySet(1,2,3) = buildSet(seq(1,2,3),[]
-    = buildSet(seq(2,3)
+    = buildSet(seq(2,3),[] +1)
+    = buildSet(seq(3), [1] +2
+    = buildSet(seq(), [1，2] +3
+    = [1,2,3]
    */
   // With the vararg A* we could provide multiple values of type A
   def apply[A](values: A*): MySet[A] = {
@@ -90,4 +164,19 @@ object MySet {
     // scala 2.12.x use `values.toSeq`
     buildSet(values.to(Seq), new EmptySet[A])
   }
+}
+
+object MySetPlayground extends App {
+  val s = MySet(1, 2, 3, 4)
+  s + 5 ++ MySet(-1, -2) + 3 flatMap (x => MySet(x, 10 * x)) filter (_ % 2 == 0) foreach println
+
+  val negative = !s // s.unary_! = all the naturals not equal to 1,2,3,4
+  println(negative(2))
+  println(negative(5))
+
+  val negativeEven = negative.filter(_ % 2 == 0)
+  println(negativeEven(5))
+
+  val negativeEven5 = negativeEven + 5 // all the even numbers > 4 + 5
+  println(negativeEven5(5))
 }
