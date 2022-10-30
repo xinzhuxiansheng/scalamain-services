6d8a227b0c28b7c82e0ceef68edaf41943344892
阿洋 <xinzhuxiansheng@126.com>
2022-10-30 18:52:05 +0800

    add

--- scala-for-the-impatient/src/main/scala/com/yzhou/chapter21/Fraction.scala
+++ scala-for-the-impatient/src/main/scala/com/yzhou/chapter21/Fraction.scala
@@ -21,13 +21,25 @@ object Fraction {
 }
 
 object Main extends App {
-  println(Fraction(4, 5))
+  //  println(Fraction(4, 5))
 
   implicit def int2Fraction(n: Int) = Fraction(n, 1)
 
-  val result = 3 * Fraction(4, 5)
+  /*
+    1. 一般情况 *(乘号)只能用于 number，e.g ： 3 * 3
+    2. 那当出现 3 * other type的时候，这足以说明，number是无法做到的，所以利用 implicit定义一个方法
+    用来将 3 -> Franction(n,1) 。
+    implicit def int2Fraction(n: Int) = Fraction(n, 1)
+    3. 那为什么能将3装换成Fraction类型，这里考虑到一点，隐式缓缓的作用域
+    int2Fraction(n: Int)方法的形参n是Int类型，当在Main class声明时，
+    scala会将该类下所有Int类型 调用int2Fraction函数转成Fraction(n,1)类型,
+    并且 Fraction类型也 定义了 *()，所以 3 * Fraction(4,5) 调用的是Fraction的*()
+
+  */
+  val result = 3 * Fraction(4, 5) // 3 => Calls int2Fraction(3)
+
   println(result)
 
-  val a = 3 * 3
-  println(a)
+  //  val a = 3 * 3
+  //  println(a)
 }
