803d52df2e193fcb60016ca9e42bd26a42213484
阿洋 <xinzhuxiansheng@126.com>
2022-11-02 10:11:54 +0800

    add

--- zio-courses/src/main/scala/com/rockthejvm/utils/Utils.scala
+++ zio-courses/src/main/scala/com/rockthejvm/utils/Utils.scala
@@ -5,6 +5,6 @@ import zio.ZIO
 extension[R, E, A] (zio: ZIO[R, E, A])
   def debugThread: ZIO[R, E, A] =
     zio
-      .tap(a => ZIO.succeed(println(s"[${Thread.currentThread().getName} $a]")))
+      .tap(a => ZIO.succeed(println(s"[${Thread.currentThread().getName}] $a")))
       .tapErrorCause(cause => ZIO.succeed(println(s"[${Thread.currentThread().getName}][FAIL] $cause")))
 
